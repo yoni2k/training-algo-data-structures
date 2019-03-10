220cf4960976b51ec1de79c0960f8f176032c862
yoni2k <yoni.k81@gmail.com>
2019-03-10 15:11:05 +0200

    Source: Cracking p.91, 1.8
    Write an algorithm such that if an element in an MxN matrix is 0, it's entire row and column are set to 0.
    
    Great implementation: no additional space, O(MxN).
    
    Implementation tool: must start from the end to check, otherwise orverridding what's being checked based on

---
+++ src/YK_ZeroMatrix.java
@@ -0,0 +1,98 @@
+/*
+Source: Cracking p.91, 1.8
+
+Write an algorithm such that if an element in an MxN matrix is 0, it's entire row and column are set to 0.
+
+Great implementation: no additional space, O(MxN).
+
+Implementation tool: must start from the end to check, otherwise orverridding what's being checked based on
+
+ */
+
+import java.util.Arrays;
+
+public class YK_ZeroMatrix {
+
+    private boolean bFirstColumnHasZero = false;
+    private int[][] gMatrix = null;
+
+    private void setFlag(int i, int j) {
+        System.out.println("Setting frag for ["+ i +"]["+j+"]");
+        System.out.println("Setting 0 in ["+ i +"][0]");
+        gMatrix[i][0] = 0;
+        if (j == 0) {
+            System.out.println("Setting bFirstColumnHasZero");
+            bFirstColumnHasZero = true;
+        }
+        else {
+            System.out.println("Setting 0 in [0]["+ j +"]");
+            gMatrix[0][j] = 0;
+        }
+    }
+
+    private boolean isZero(int i, int j) {
+        if (j == 0) {
+            System.out.println("isZero, J==0!!! -  ["+i+"]["+j+"], matrix[i][0]: " + gMatrix[i][0] + ", bFirstColumnHasZero: " + bFirstColumnHasZero);
+            return ((gMatrix[i][0] == 0) || bFirstColumnHasZero);
+        }
+        else {
+            System.out.println("isZero, ["+i+"]["+j+"], matrix[i][0]: " + gMatrix[i][0] + ", matrix[0][j]: " + gMatrix[0][j]);
+            return ((gMatrix[i][0] == 0) || (gMatrix[0][j] == 0));
+        }
+    }
+
+    public int[][] zeroMatrix(int[][] matrix) {
+
+        if(matrix == null)
+            return null;
+
+        System.out.println("Zeroing matrix: " + Arrays.deepToString(matrix));
+
+        int M = matrix.length;
+        int N = matrix[0].length;
+
+        gMatrix = matrix.clone();
+
+        //implementation without using extra room besides the matrix + 1 more room for (0,0)
+        for(int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if(matrix[i][j] == 0) {
+                    setFlag(i,j);
+                }
+            }
+        }
+
+        for(int i = M-1; i >= 0; i--) {
+            for (int j = N-1; j >= 0; j--) {
+                if(isZero(i, j)) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        /* previous implementation with extra M + N room
+        boolean[] mZeros = new boolean[M];
+        boolean[] nZeros = new boolean[N];
+
+        for(int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if(matrix[i][j] == 0) {
+                    mZeros[i] = true;
+                    nZeros[j] = true;
+                }
+            }
+        }
+
+        for(int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if(mZeros[i] || nZeros[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        } */
+
+        System.out.println("Final matrix: " + Arrays.deepToString(gMatrix));
+
+        return gMatrix;
+    }
+}
---
+++ tests/YK_ZeroMatrixTest.java
@@ -0,0 +1,87 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class YK_ZeroMatrixTest {
+
+    int[][] build2x1Zeros = {{0},{0}};
+    int[][] build1x2Zeros = {{0,0}};
+    int[][] build4x3Zeros = {{0,0,0}, {0,0,0}, {0,0,0}, {0,0,0}};
+
+    @Test
+    void smallMatrixes() {
+
+        assertTrue(Arrays.deepEquals(null,(new YK_ZeroMatrix()).zeroMatrix(null)));
+
+        int[][] mat1x1Without = {{1}};
+        int[][] mat1x1With = {{0}};
+
+        assertTrue(Arrays.deepEquals(mat1x1Without,(new YK_ZeroMatrix()).zeroMatrix(mat1x1Without)));
+        assertTrue(Arrays.deepEquals(mat1x1With,(new YK_ZeroMatrix()).zeroMatrix(mat1x1With)));
+
+        int[][] mat2x1Without = {{1},{1}};
+        int[][] mat2x1With1st = {{0},{1}};  //1st place
+        int[][] mat2x1WithLast = {{1},{0}};  //last place
+
+        assertTrue(Arrays.deepEquals(mat2x1Without,(new YK_ZeroMatrix()).zeroMatrix(mat2x1Without)));
+        assertTrue(Arrays.deepEquals(build2x1Zeros,(new YK_ZeroMatrix()).zeroMatrix(mat2x1With1st)));
+        assertTrue(Arrays.deepEquals(build2x1Zeros,(new YK_ZeroMatrix()).zeroMatrix(mat2x1WithLast)));
+
+        int[][] mat1x2Without = {{1,1}};
+        int[][] mat1x2With1st = {{0,1}};  //1st place
+        int[][] mat1x2WithLast = {{1,0}};  //last place
+
+        assertTrue(Arrays.deepEquals(mat1x2Without,(new YK_ZeroMatrix()).zeroMatrix(mat1x2Without)));
+        assertTrue(Arrays.deepEquals(build1x2Zeros,(new YK_ZeroMatrix()).zeroMatrix(mat1x2With1st)));
+        assertTrue(Arrays.deepEquals(build1x2Zeros,(new YK_ZeroMatrix()).zeroMatrix(mat1x2WithLast)));
+    }
+
+    @Test
+    void mat4x3Without() {
+        int[][] mat4x3Without = {{1,2,3}, {4,5,6}, {7,8,9}, {10,11,12}};
+
+        assertTrue(Arrays.deepEquals(mat4x3Without,(new YK_ZeroMatrix()).zeroMatrix(mat4x3Without)));
+    }
+
+    @Test
+    void mat4x3Corners() {
+        int[][] mat4x3CornerTL = {{0,2,3}, {4,5,6}, {7,8,9}, {10,11,12}};
+        int[][] exp_mat4x3CornerTL = {{0,0,0}, {0,5,6}, {0,8,9}, {0,11,12}};
+
+        int[][] mat4x3CornerTR = {{1,2,0}, {4,5,6}, {7,8,9}, {10,11,12}};
+        int[][] exp_mat4x3CornerTR = {{0,0,0}, {4,5,0}, {7,8,0}, {10,11,0}};
+
+        int[][] mat4x3CornerBL = {{1,2,3}, {4,5,6}, {7,8,9}, {0,11,12}};
+        int[][] exp_mat4x3CornerBL = {{0,2,3}, {0,5,6}, {0,8,9}, {0,0,0}};
+
+        int[][] mat4x3CornerBR = {{1,2,3}, {4,5,6}, {7,8,9}, {10,11,0}};
+        int[][] exp_mat4x3CornerBR = {{1,2,0}, {4,5,0}, {7,8,0}, {0,0,0}};
+
+        assertTrue(Arrays.deepEquals(exp_mat4x3CornerTL,(new YK_ZeroMatrix()).zeroMatrix(mat4x3CornerTL)));
+        assertTrue(Arrays.deepEquals(exp_mat4x3CornerTR,(new YK_ZeroMatrix()).zeroMatrix(mat4x3CornerTR)));
+        assertTrue(Arrays.deepEquals(exp_mat4x3CornerBL,(new YK_ZeroMatrix()).zeroMatrix(mat4x3CornerBL)));
+        assertTrue(Arrays.deepEquals(exp_mat4x3CornerBR,(new YK_ZeroMatrix()).zeroMatrix(mat4x3CornerBR)));
+    }
+
+    @Test
+    void mat4x3Middle() {
+        int[][] mat4x3Middle = {{1,2,3}, {4,0,6}, {7,8,9}, {10,11,12}};
+        int[][] exp_mat4x3Middle = {{1,0,3}, {0,0,0}, {7,0,9}, {10,0,12}};
+
+        int[][] mat4x3Many = {{0,2,3}, {4,0,6}, {7,8,9}, {0,11,12}};
+        int[][] exp_mat4x3Many = {{0,0,0}, {0,0,0}, {0,0,9}, {0,0,0}};
+
+        assertTrue(Arrays.deepEquals(exp_mat4x3Middle,(new YK_ZeroMatrix()).zeroMatrix(mat4x3Middle)));
+        assertTrue(Arrays.deepEquals(exp_mat4x3Many,(new YK_ZeroMatrix()).zeroMatrix(mat4x3Many)));
+    }
+
+    @Test
+    void mat4x3AllBecomeZeros() {
+        int[][] mat4x3AllBecomeZeros = {{0,2,3}, {4,0,6}, {7,8,0}, {10,11,12}};
+
+        assertTrue(Arrays.deepEquals(build4x3Zeros,(new YK_ZeroMatrix()).zeroMatrix(mat4x3AllBecomeZeros)));
+        assertTrue(Arrays.deepEquals(build4x3Zeros,(new YK_ZeroMatrix()).zeroMatrix(build4x3Zeros)));
+    }
+}
