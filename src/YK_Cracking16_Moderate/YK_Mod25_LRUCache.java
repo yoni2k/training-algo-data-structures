9b60948b645b9b3f349ee5a8ff4884f56e4fe883
yoni2k <yoni.k81@gmail.com>
2019-04-22 18:04:28 +0300

    Source: Cracking 16.25 Implement a cache with limited space. Actions: add, lookup, update. Every time a space runs out, remove oldest not used (added/looked up/updated) item.
    Implementations:
    - Initial implementation before improvements from the book (see history in github):
        Insert - O(1)
        Retrieve - O(1) - but actually there is a bug that retrieval doesn't change priority
        Update - O(n)
    - Implementation after reading the book with update also with O(1) for retrieve and update.

--- src/YK_Cracking16_Moderate/YK_Mod25_LRUCache.java
+++ src/YK_Cracking16_Moderate/YK_Mod25_LRUCache.java
@@ -1,7 +1,8 @@
 package YK_Cracking16_Moderate;
 
+import YK_Common.YK_Exception;
+
 import java.util.HashMap;
-import java.util.LinkedList;
 
 /*
 Source: Cracking 16.25
@@ -9,35 +10,137 @@ Implement a cache with limited space.
 Actions: add, lookup, update.
 Every time a space runs out, remove oldest not used (added/looked up/updated) item.
 
-Initial implementation before improvements from the book
+Implementations:
+- Initial implementation before improvements from the book (see history in github):
+    Insert - O(1)
+    Retrieve - O(1) - but actually there is a bug that retrieval doesn't change priority
+    Update - O(n)
+- Implementation after reading the book with update also with O(1) for retrieve and update.
  */
 
 public class YK_Mod25_LRUCache {
-    private HashMap<Integer,Integer> map;
-    private LinkedList<Integer> list;
+
+    class Node {
+        int key;
+        int value;
+        Node next = null;
+        Node prev = null;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        void remove() {
+            if(prev != null) {
+                prev.next = next;
+            }
+            if(next != null) {
+                next.prev = prev;
+            }
+        }
+
+        void insertEnd(Node newNode) {
+            if(next != null) {
+                throw  new YK_Exception("Node not last, key: " + key + ", value: " + value);
+            }
+            next = newNode;
+            newNode.prev = this;
+            newNode.next = null;
+        }
+
+        String printAll() {
+            Node node = this;
+            StringBuilder sb = new StringBuilder();
+
+            while(node != null) {
+                sb.append(" [");
+                sb.append(node.key);
+                sb.append(',');
+                sb.append(node.value);
+                sb.append(']');
+                node = node.next;
+            }
+            return sb.toString();
+        }
+
+        String printBack() {
+            Node node = this;
+            StringBuilder sb = new StringBuilder();
+
+            while(node != null) {
+                sb.append(" [");
+                sb.append(node.key);
+                sb.append(',');
+                sb.append(node.value);
+                sb.append(']');
+                node = node.prev;
+            }
+            return sb.toString();
+        }
+    }
+
+    private HashMap<Integer,Node> map;
+    private Node first;
+    private Node last;
     private int maxSize;
 
     public YK_Mod25_LRUCache(int maxSize) {
         map = new HashMap<>();
-        list = new LinkedList<>();
+        first = null;
+        last = null;
         this.maxSize = maxSize;
+    }
+
+    private void removeFirst() {
+        Node nodeToRemove = first;
+        first = nodeToRemove.next;
+        nodeToRemove.remove();
+        map.remove(nodeToRemove.key);
     }
 
     public void insert(int key, int value) {
         if(map.size() == maxSize) {
-            int keyToRemove = list.remove();//remove first
-            map.remove(keyToRemove);
+            removeFirst();
         }
-        map.put(key, value);
-        list.add(key);//add to the end
+        Node newNode = new Node(key, value);
+        if(last == null) {
+            last = newNode;
+            first = newNode;
+        } else {
+            last.insertEnd(newNode);
+            last = newNode;
+        }
+        map.put(key, newNode);
+    }
+
+    private void moveToEnd(Node nodeToMove) {
+        if(nodeToMove != last) {
+            if(nodeToMove == first) {
+                first = nodeToMove.next;
+            }
+            nodeToMove.remove();
+            last.insertEnd(nodeToMove);
+            last = nodeToMove;
+        }
     }
 
     public int retrieve(int key) {
-        return map.get(key);
+        Node node = map.get(key);
+        moveToEnd(node);
+        return node.value;
     }
 
     public void update(int key, int value) {
-        list.remove((Integer)key);
-        insert(key, value);
+        Node node = map.get(key);
+        moveToEnd(node);
+        node.value = value;
+    }
+
+    public void printList() {
+        System.out.println("Printing list: ----------------------------------");
+        System.out.println("First: [" + first.key + "," + first.value + "], last: [" + last.key + "," + last.value + "]");
+        System.out.println("List from first: " + first.printAll());
+        System.out.println("List from last: " + last.printBack());
     }
 }
--- tests/YK_Cracking16_Moderate/YK_Mod25_LRUCacheTest.java
+++ tests/YK_Cracking16_Moderate/YK_Mod25_LRUCacheTest.java
@@ -9,32 +9,30 @@ class YK_Mod25_LRUCacheTest {
     void simple() {
         YK_Mod25_LRUCache cache = new YK_Mod25_LRUCache(3);
 
-        cache.insert(1,11);
-        cache.insert(2,12);
-        cache.insert(3,13);
+        cache.insert(1,11); cache.printList();
+        cache.insert(2,12); cache.printList();
+        cache.insert(3,13); cache.printList();
 
-        assertEquals(11, cache.retrieve(1));
-        assertEquals(12, cache.retrieve(2));
-        assertEquals(13, cache.retrieve(3));
+        assertEquals(11, cache.retrieve(1));cache.printList();
+        assertEquals(12, cache.retrieve(2));cache.printList();
+        assertEquals(13, cache.retrieve(3));cache.printList();
 
-        cache.insert(4,14);
+        cache.insert(4,14);cache.printList();
 
-        assertEquals(12, cache.retrieve(2));
-        assertEquals(13, cache.retrieve(3));
-        assertEquals(14, cache.retrieve(4));
+        assertEquals(12, cache.retrieve(2));cache.printList();
+        assertEquals(13, cache.retrieve(3));cache.printList();
+        assertEquals(14, cache.retrieve(4));cache.printList();
 
-        assertThrows(NullPointerException.class, () -> cache.retrieve(1));
+        assertThrows(NullPointerException.class, () -> cache.retrieve(1));cache.printList();
 
-        cache.update(2, 22);
-        cache.insert(5,15);
+        cache.update(2, 22);cache.printList();
+        cache.insert(5,15);cache.printList();
 
-        assertEquals(14, cache.retrieve(4));
-        assertEquals(22, cache.retrieve(2));
-        assertEquals(15, cache.retrieve(5));
+        assertEquals(14, cache.retrieve(4));cache.printList();
+        assertEquals(22, cache.retrieve(2));cache.printList();
+        assertEquals(15, cache.retrieve(5));cache.printList();
 
-        assertThrows(NullPointerException.class, () -> cache.retrieve(3));
-
-
+        assertThrows(NullPointerException.class, () -> cache.retrieve(3));cache.printList();
     }
 
 }
