b8dbb9591dfb4749444b681f4310216b955835b7
yoni2k <yoni.k81@gmail.com>
2019-05-02 20:00:50 +0300

    Implementation with memorization. Every pass is O(n),     next pass is at worst case 1/2 n (the other half we either fill right away, or get with memorization.,     therefore complexity is O(n log n).     Space complexity O(n log n) - we put memotization for every point we touch

--- src/YK_Cracking17_Hard/YK_Hard21_VolumeHistogram.java
+++ src/YK_Cracking17_Hard/YK_Hard21_VolumeHistogram.java
@@ -4,8 +4,10 @@ package YK_Cracking17_Hard;
 Source: Cracking 17.21
 Given a histogram (see book), check how much water will be in it
 
-Implementation with memorization - much better.  Also simplified logic, always filling 1 part, and doing 2 recursions,
-    one of them is going to be O(1) because of memorization, gives O(n log n)
+Implementation with memorization. Every pass is O(n),
+    next pass is at worst case 1/2 n (the other half we either fill right away, or get with memorization.,
+    therefore complexity is O(n log n).
+    Space complexity O(n log n) - we put memotization for every point we touch
  */
 
 import java.awt.*;
@@ -13,17 +15,10 @@ import java.util.HashMap;
 
 public class YK_Hard21_VolumeHistogram {
 
-    private class YK_Pair {
-        int one;
-        int two;
-
-        YK_Pair(int one, int two) {
-            this.one = one;
-            this.two = two;
-        }
-    }
-
     private int fill(int[] hist, int start, int end) {
+        if(start > (end - 2)) {
+            return 0;
+        }
         int height = Integer.min(hist[start], hist[end]);
         int water = (end - start - 1) * height;
 
@@ -35,59 +30,52 @@ public class YK_Hard21_VolumeHistogram {
         return water;
     }
 
-    private int[] get2MaxIndexes(int[] hist, int start, int end, HashMap<Point, YK_Pair> memMaxesInds) {
+    private int getMaxInd(int[] hist, int start, int end, HashMap<Point, Integer> memMaxInds) {
         Point p = new Point(start, end);
-        YK_Pair maxesIndexes;
-        if(memMaxesInds.containsKey(p)) {
-            maxesIndexes = memMaxesInds.get(p);
-            return new int[] {maxesIndexes.one, maxesIndexes.two};
+        if(start == end) {
+            return hist[start];
+        } else if(memMaxInds.containsKey(p)) {
+            return memMaxInds.get(p);
         }
 
-        int indMax =        (hist[start] >= hist[start+1]) ? start          : start + 1;
-        int max =           (hist[start] >= hist[start+1]) ? hist[start]    : hist[start + 1];
-        int maxSecond =     (hist[start] >= hist[start+1]) ? hist[start + 1]: hist[start];
-        int indSecondMax;
+        int max = hist[start];
+        int ind = start;
 
-        maxesIndexes = new YK_Pair(start, start + 1);
-        memMaxesInds.put(new Point(start, start + 1), maxesIndexes);
-
-        for(int i = start + 2; i <= end; i++) {
+        for(int i = start + 1; i <= end; i++) {
             if(hist[i] > max) {
                 max = hist[i];
-                indSecondMax = indMax;
-                indMax = i;
-                maxesIndexes = new YK_Pair(Integer.min(indMax, indSecondMax), Integer.max(indMax, indSecondMax));
-            } else if((indMax == start) && (hist[i] > maxSecond)) {
-                maxSecond = hist[i];
-                indSecondMax = i;
-                maxesIndexes = new YK_Pair(Integer.min(indMax, indSecondMax), Integer.max(indMax, indSecondMax));
+                ind = i;
             }
-            memMaxesInds.put(new Point(start, i), maxesIndexes);
+            memMaxInds.put(new Point(start, i), ind);
         }
-        System.out.print("get2MaxIndexes FINISH - start: " + start + ", end: " + end + ", return: ");
-        System.out.println("[" + maxesIndexes.one + "," + maxesIndexes.two + "]");
-        return new int[] {maxesIndexes.one, maxesIndexes.two};
+        return ind;
     }
 
-    private int getWater(int[] hist, int start, int end, HashMap<Point, YK_Pair> memMaxsIndexes) {
+    private int getWater(int[] hist, int start, int end, HashMap<Point, Integer> memMaxInds) {
         System.out.println("getWater START, start: " + start + ", end: " + end);
         if((start >= hist.length) || ((end - start) < 2)) {
             return 0;
         }
-        int[] maxsIndexes = get2MaxIndexes(hist, start, end, memMaxsIndexes);
+
         int water = 0;
-
-        water += fill(hist, maxsIndexes[0], maxsIndexes[1]);
-        water += getWater(hist, start, maxsIndexes[0], memMaxsIndexes);
-        water += getWater(hist, maxsIndexes[1], end, memMaxsIndexes);
-
+        int maxInd = getMaxInd(hist, start, end, memMaxInds);
+        if(maxInd < (end - 2)) {
+            int maxRightInd = getMaxInd(hist, maxInd + 1, end, memMaxInds);
+            water += fill(hist, maxInd, maxRightInd);
+            water += getWater(hist, maxRightInd, end, memMaxInds);
+        }
+        if(maxInd > (start + 2)) {
+            int maxLeftInd = getMaxInd(hist, start, maxInd - 1, memMaxInds);
+            water += fill(hist, maxLeftInd, maxInd);
+            water += getWater(hist, start, maxLeftInd, memMaxInds);
+        }
         System.out.println("getWater END, start: " + start + ", end: " + end + ", water: " + water);
         return water;
 
     }
 
     public int getWaterHistogram(int[] hist) {
-        HashMap<Point, YK_Pair> memMaxsIndexes = new HashMap<>();
-        return getWater(hist, 0, hist.length - 1, memMaxsIndexes);
+        HashMap<Point, Integer> memMaxInds = new HashMap<>();
+        return getWater(hist, 0, hist.length - 1, memMaxInds);
     }
 }
